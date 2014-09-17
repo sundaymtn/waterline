c1de03b08ee78317d7aa880bee58a70f72b49e8d
Seth Carter <seth.carter@cd-adapco.com>
2014-09-17 11:43:50 -0400

    branch changes

--- waterline.py
+++ waterline.py
@@ -108,12 +108,10 @@ serverStart.execute()
 print "Stopping Redis"
 
 
-addUpdatedDb = process.ProcessClass(exec_list=([r'git remote set-url origin https://username:669288a22a7ba23a44fc088f9442deb5b299a03e@github.com/sundaymtn/waterline.git'],
-                                               [r'git status'],
-                                               [r'git checkout database'],
+addUpdatedDb = process.ProcessClass(exec_list=([r'git status'],
                                                [r'git add waterline.rdb'],
                                                [r'git commit -m "updated waterline data"'],
-                                               [r'git push']), out=True, limit_response=0, errors_expected=False,
+                                               [r'git push -u origin database']), out=True, limit_response=0, errors_expected=False,
                            return_proc=False, use_call=False, use_shell=True, environ=None)
 ret = addUpdatedDb.execute()
 print 'Pushing updated redis db to remote'
