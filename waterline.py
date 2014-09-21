b35bbd7017ab59de3d198b11973a8cd206ab8cfd
Seth Carter <sundaymtn@gmail.com>
2014-09-21 19:08:02 +0000

    adding expected

--- waterline.py
+++ waterline.py
@@ -8,7 +8,12 @@ import pickle
 import plotly.plotly as py
 from plotly.graph_objs import Data, Scatter 
 import process
+import sys
 
+poll = True
+log = True
+plot = True
+push = False
 
 serverStart = process.ProcessClass(exec_list=([r'redis-server', './redis.conf'],), out=True, limit_response=0, errors_expected=False,
                            return_proc=True, use_call=False, use_shell=False, environ=None)
@@ -26,17 +31,20 @@ def get_value(redis, key):
     return pickle.loads(pickled_value)
 
 
-siteKeys = ['335122','335121','335123','335124', '335125', '335126']
+siteKeys = ['335124', '335125', '335126']
 location = {}
 
 
-# r.flushdb()
+#r.flushdb()
 
 
 for siteKey in siteKeys:
-    response = urllib2.urlopen('http://www.h2oline.com/default.aspx?pg=si&op=%s' % siteKey)
-    html = response.read()
-    r.set(siteKey, html)
+    if poll:
+        print 'http://www.h2oline.com/default.aspx?pg=si&op=%s' % siteKey
+        response = urllib2.urlopen('http://www.h2oline.com/default.aspx?pg=si&op=%s' % siteKey)
+        html = response.read()
+        r.set(siteKey, html)
+        time.sleep(5)
     cfs = get_value(r, siteKey + '-cfs')
     if not cfs:
         cfs = {}
@@ -45,7 +53,7 @@ for siteKey in siteKeys:
     try:
         location[siteKey] = locationSpan[0].get_text().split(siteKey)[1].lstrip()
     except:
-        print siteKey,locationSpan,html
+        print siteKey,locationSpan,cfs
         
     timeSpan = soup.span(id = 'Footer-C')
     match = re.search(r'([A-Z]{3}).(\d{2}),.(\d{4}).AT.\d{2}:\d{2}:\d{2}', timeSpan[0].get_text().lstrip())
@@ -73,33 +81,84 @@ for siteKey in siteKeys:
 flowList = []
 for siteKey in siteKeys:
     damName = location[siteKey].split('AT')[1].lstrip()
-    print siteKey, damName
+    if log:
+        print siteKey, damName
     histFlow = get_value(r, siteKey + '-cfs')
-    x = []
-    y = []
+    xActualDate = []
+    yActualFlow = []
+    xExpectedDate = []
+    yExpectedFlow = []
     sortedDates = histFlow.keys()
     sortedDates.sort()
-
-    for flowDate in sortedDates:
+    datePoints = len(sortedDates)
+    for datePoint in xrange(datePoints):
+        flowDate = sortedDates[datePoint]
         flowData = histFlow[flowDate]
-        
-        print '\t'+flowDate.strftime('%b %d %Y %H:%M:%S')
-        x.append(flowDate)
+        if log:
+            print '\t'+flowDate.strftime('%b %d %Y %H:%M:%S')
+        xActualDate.append(flowDate)
         for fD in flowData:
-            print '\t  ' + fD
+            tomorrow = False
+            if log:
+                print '\t  ' + fD
             if fD.startswith('At'):
-                y.append(fD.split(' ')[-1])
-    flowList += [Scatter(name = damName ,x = x, y = y, fill ="tozeroy")]
+                yActualFlow.append(fD.split(' ')[-1])
+            if fD.startswith('From'):
+                lineItemFrom = fD.split(' ')
+                if datePoint+1 == datePoints:
+                    yExpectedFlow.append(lineItemFrom[-1])
+                
+                if lineItemFrom[1] == 'MIDNIGHT':
+                    lineItemFrom[1] = '23:59'
+                    
+                fromTime = [int(x) for x in lineItemFrom[1].split(':')]
+                
+                if lineItemFrom[2] == 'PM':
+                    fromTime[0] = fromTime[0]+12
+                if datePoint+1 == datePoints:
+                    xExpectedDate.append(flowDate.replace(hour=fromTime[0],minute=fromTime[1]))
+                
+                
+                if 'today' in fD:
+                    fD = fD.split('today ')[1]
+                if 'tomorrow' in fD:
+                    try:
+                        fD = fD.split('tomorrow ')[1]
+                    except:
+                        fD = fD.split('tomorrow, ')[1]
+                    tomorrow = True
+            if fD.startswith(('Until','until')):
+                lineItemUntil = fD.split(' ')
+                yExpectedFlow.append(lineItemUntil[-1])
+                
+                if lineItemUntil[1] == 'MIDNIGHT':
+                    lineItemUntil[1] = '23:59'
+                    
+                untilTime = [int(x) for x in lineItemUntil[1].split(':')]
+                
+                if lineItemUntil[2] == 'PM':
+                    untilTime[0] = untilTime[0]+12
+                if tomorrow:
+                    flowDateUntil = flowDate + datetime.timedelta(days=1)
+                else:
+                    flowDateUntil = flowDate
+                xExpectedDate.append(flowDateUntil.replace(hour=untilTime[0],minute=untilTime[1]))
+                
+    flowList += [Scatter(name = damName+'-expected' ,x = xExpectedDate, y = yExpectedFlow, fill ="none", line={"dash":"dot"})]
+                
+    flowList += [Scatter(name = damName ,x = xActualDate, y = yActualFlow, fill ="tozeroy")]
     
 data = Data(flowList)
 riverName = damName = location[siteKey].split('AT')[0].rstrip()
-py.sign_in('sundaymtn','kfw4lbn1wt')
-unique_url = py.plot(data, filename = riverName, auto_open=False, overwrite=True)
+
+if plot:
+    py.sign_in('sundaymtn','kfw4lbn1wt')
+    unique_url = py.plot(data, filename = riverName+"-test", auto_open=False, overwrite=True)
 
 serverStop = process.ProcessClass(exec_list=([r'redis-cli', 'shutdown'],), out=True, limit_response=0, errors_expected=False,
                            return_proc=False, use_call=False, use_shell=False, environ=None)
-serverStart.execute()
 print "Stopping Redis"
+serverStart.execute()
 
 
 addUpdatedDb = process.ProcessClass(exec_list=([r'git remote set-url origin https://sundaymtn:669288a22a7ba23a44fc088f9442deb5b299a03e@github.com/sundaymtn/waterline.git'],
@@ -112,11 +171,11 @@ addUpdatedDb = process.ProcessClass(exec_list=([r'git remote set-url origin http
                                                [r'git commit -m "[skip ci] updated waterline data"'],
                                                [r'git push']), out=True, limit_response=0, errors_expected=False,
                            return_proc=False, use_call=False, use_shell=True, environ=None)
-
-print 'Pushing updated redis db to remote'
-ret = addUpdatedDb.execute()
-for line in ret:
-    print '\t'+line
+if push:
+    print 'Pushing updated redis db to remote'
+    ret = addUpdatedDb.execute()
+    for line in ret:
+        print '\t'+line
 
 
 
